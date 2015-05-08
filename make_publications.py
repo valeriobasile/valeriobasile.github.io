c56849c9340c1d496fa72465ec468a76ba9f7da7
Valerio Basile <valeriobasile@gmail.com>
2015-05-08 14:56:48 +0200

    added publications list and script to convert from Scholar

---
+++ make_publications.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import bibtexparser
+
+with open('publications.bib') as bibtex_file:
+    bib_database = bibtexparser.load(bibtex_file)
+    
+for bib_item in bib_database.entries:
+#    print bib_item
+    if 'booktitle' in bib_item:
+        venue = bib_item['booktitle']
+    elif bib_item['journal']:
+        venue = bib_item['journal']
+    else:
+        venue = ''
+
+    if 'pages' in bib_item:
+        pages = ', {0}'.format(bib_item['pages'])
+    else:
+        pages = ''
+
+    print ("* {0} *{1}* ({2}), {3}, {4}".format(bib_item['author'],
+                                                bib_item['title'],
+                                                bib_item['year'],
+                                                venue,
+                                                pages))
+
